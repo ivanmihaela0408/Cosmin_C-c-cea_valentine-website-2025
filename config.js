355f06531a23dbf597e448fee87d37eed79494c5
ivanmihaela0408 <ivanmihaela0408@gmail.com>
2026-01-28 21:02:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: https://res.cloudinary.com/dae9hqlhv/video/upload/v1769625346/inima-mea-e-locul-tau--live-session-w-band_wueklk.mp3, // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dae9hqlhv/video/upload/v1769625346/inima-mea-e-locul-tau--live-session-w-band_wueklk.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
